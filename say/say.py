1cfcd2cc621ba917c2918c0a2a5e912aaedf7391
#!/bin/cat <42851999+gamer4life1@users.noreply.github.com>
2022-09-22 08:53:31 -0700

    fixed indentation

--- say/say.py
+++ say/say.py
@@ -100,7 +100,7 @@ class Say(BaseCog):
                     exc_info=e,
                 )
 
-     @commands.command(name="say")
+    @commands.command(name="say")
 #     @checks.admin_or_permissions(administrator=True)
     async def _say(
         self, ctx: commands.Context, channel: Optional[discord.TextChannel], *, text: str = ""
